3fd5bcbc18bee0616d495f1f37544f2a41f14c8a
Anton Kovalev <antoha91011@mail.ru>
2023-10-04 12:40:25 +0700

    Итоговая контрольная работа по блоку специализация

---
+++ History_Comand.java
@@ -0,0 +1,41 @@
+// Задание 1
+
+// mkdir FW_2
+// cd FW_2
+// cat > home_animals
+// cat > pack_animals
+// cat home_animals pack_animals > animals
+// cat animals
+// mv animals mans_friends
+// ls -ali
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+
+// Задание 2
+
+// cd ..
+// mkdir FW_2_system
+// cd Kennel
+// mv mans_friends ~/FW_2_system
+// cd FW_2_system
+// ls -ali
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+// Задание 3
+
+// sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.23-1_all.deb
+// sudo dpkg -i mysql-apt-config_0.8.23-1_all.deb
+// sudo apt-get update
+// sudo apt-get install mysql-server
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+// Задание 4
+
+// sudo wget https://download.docker.com/linux/ubuntu/dists/jammy/pool/stable/amd64/docker-ce-cli_20.10.13~3-0~ubuntu-jammy_amd64.deb
+// sudo dpkg -i docker-ce-cli_20.10.133-0ubuntu-jammy_amd64.deb
+// sudo dpkg -r docker-ce-cli
+
+//-----------------------------------------------------------------------------------------------------------------------------------
